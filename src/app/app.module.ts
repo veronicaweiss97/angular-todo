0dbdf60982096b6040c64abd29cb1d6a12e3f984
Veronica <veronicaweiss77@gmail.com>
2021-11-30 14:16:04 +0100

    animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { WindowComponent } from './components/window/window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/window/window.component.ts
@@ -0,0 +1,74 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-window',
+  templateUrl: './window.component.html',
+  styleUrls: ['./window.component.css'],
+  animations: [
+    trigger('fade', [
+      state('void', style({opacity: 0})),
+      transition('void => *', [
+
+        animate(2000)
+      ]),
+      //* means default state
+      transition('* => void',[
+        animate(1000)
+      ] )
+    ]),
+    trigger('slide', [
+      transition('void => *', [
+        style({transform: 'translateX(20px)'}),
+        animate(1000)
+      ])
+    ]),
+    trigger('appear', [
+      transition('void => *', [
+        style({transform: 'translateY(-80px)'}),
+        animate(1000)
+      ]),
+    ])
+  ],
+
+})
+
+export class WindowComponent implements OnInit {
+
+  @Output() onRemove = new EventEmitter()
+
+  inputValue: string = ''
+  todos: any = [
+    {title: 'Buy bread', idx: Date.now(), isChecked: false},
+
+  ]
+
+  myForm : FormGroup = new FormGroup({
+    "title": new FormControl('', Validators.required),
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  getInputValue(text: any) {
+    this.inputValue = text.target.value
+  }
+  addTodo(event: any) {
+    event.preventDefault()
+    if(this.inputValue) {
+      this.todos.push({title: this.inputValue, idx: Date.now(), isChecked: false})
+    this.inputValue = ''
+    }
+
+  }
+  onRemoveItem(idx: number) {
+    const before = this.todos.slice(0, idx)
+    const after = this.todos.slice(idx + 1)
+    const newArr = [...before, ...after]
+    this.todos = newArr
+
+  }
+}
